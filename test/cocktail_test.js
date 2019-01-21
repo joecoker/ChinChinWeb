2b7e2944e66a938bc4cecfeadecff0f632ba1815
Al <>
2019-01-21 14:22:09 +0000

    Change findAll method & test to only return names

--- test/cocktail_test.js
+++ test/cocktail_test.js
@@ -4,19 +4,38 @@ const Cocktail = require('../lib/cocktail');
 describe('Cocktail', function() {
 
   describe('#listAll', function() {
-    it('lists out cocktails names and ingredients', async function() {
+
+    it('lists out cocktails names', async function() {
 
       let cocktailList = await Cocktail.listAll();
 
-      expect(cocktailList[0].name).equal("Vesper");
-      expect(cocktailList[0].ingredients).to.include("Gin");
-      expect(cocktailList[0].ingredients).to.include("Vodka");
-      expect(cocktailList[0].ingredients).to.include("Lillet Blonde");
+      expect(cocktailList).to.include("Vesper");
+      expect(cocktailList).to.include("Lemon Drop Martini");
+      expect(cocktailList).to.include("Harvey Wallbanger");
 
-    })
+    });
+    //
+    // it('includes special ingredients', async function() {
+    //
+    //   let cocktailList = await Cocktail.listAll();
+    //   let tuxedo = cocktailList.find(recipe => recipe.name === "Tuxedo");
+    //
+    //   expect(tuxedo.ingredients).to.include("1/2 bar spoon Maraschino");
+    //   expect(tuxedo.ingredients).to.include("1/4 bar spoon Absinthe");
+    //   expect(tuxedo.ingredients).to.include("3 dashes Orange Bitters");
+    //
+    // });
+    //
+    // it('attaches labels, if present, to ingredients', async function() {
+    //
+    //   let cocktailList = await Cocktail.listAll();
+    //   let tuxedo = cocktailList.find(recipe => recipe.name === "Tuxedo");
+    //
+    //   expect(tuxedo.ingredients).to.include(["Gin", "Old Tom Gin"]);
+    //   expect(tuxedo.ingredients).to.include(["Vermouth", "Dry vermouth"]);
+    //
+    // });
 
   });
-
-
 
 });
